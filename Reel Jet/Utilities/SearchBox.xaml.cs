bc97c6b99c75791e1b97487a2cad999ebd58e96c
hasanhttps <hasanabdullazad@gmail.com>
2023-07-17 18:37:09 +0400

    MovieListPage Design Created

---
+++ Reel Jet/Utilities/SearchBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reel_Jet.Utilities {
+ 
+    public partial class SearchBox : UserControl {
+        public SearchBox() {
+            InitializeComponent();
+        }
+
+        public string PlaceHolder {
+            get { return (string)GetValue(PlaceHolderProperty); }
+            set { SetValue(PlaceHolderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlaceHolder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceHolderProperty =
+            DependencyProperty.Register("PlaceHolder", typeof(string), typeof(SearchBox));
+    }
+}
--- Reel Jet/ViewModels/MainViewModel.cs
+++ Reel Jet/ViewModels/MainViewModel.cs
@@ -6,13 +6,14 @@ using Reel_Jet.Views;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using Reel_Jet.Views.RegistrationPages;
+using Reel_Jet.Views.MoviePages;
 
 namespace Reel_Jet.ViewModels {
     public class MainViewModel {
         private Frame MainFrame;
         public MainViewModel(Frame frame) { 
             MainFrame = frame;
-            MainFrame.Content = new SignupPage();
+            MainFrame.Content = new MovieListPage();
         }
 
     }
